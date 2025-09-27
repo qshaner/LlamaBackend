e04b4f5561c082c58437c1860a2ccf75e50c0c06
Rob Kinyon <rob.kinyon@gmail.com>
2025-09-27 14:52:12 -0400

    Add CORS allow-all

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 
 from llamarama.myapp import simple_page
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(simple_page)
 
 if __name__ == "__main__":
